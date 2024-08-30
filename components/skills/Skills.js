0dc06c7784c5711f5161df19858c331ae394223c
Vinke <kevinmorgane@hotmail.fr>
2024-08-30 21:47:05 +0200

    fix error image sizing in skill section

--- components/skills/Skills.js
+++ components/skills/Skills.js
@@ -19,10 +19,16 @@ export const Skills = () => {
     if (skillsRef.current) {
       observer.observe(skillsRef.current);
     }
+
+    return () => {
+      if (skillsRef.current) {
+        observer.disconnect();
+      }
+    };
   }, []);
 
   const skills = [
-    { name: "Base de donnees", logo: ["/mysql.webp", "/postgress.webp"] },
+    { name: "Base de données", logo: ["/mysql.webp", "/postgress.webp"] },
     {
       name: "Langages",
       logo: ["/html.webp", "/css.webp", "/js.webp", "/C.webp", "/python.webp"],
@@ -32,13 +38,14 @@ export const Skills = () => {
       logo: ["/react.webp", "/nextjs.webp"],
     },
   ];
+
   return (
     <div className="flex py-20">
       <div className="flex flex-col gap-12 justify-center items-center align-middle w-full">
-        <h3 className="text-black text-3xl font-bold">Mes Competences</h3>
+        <h3 className="text-black text-3xl font-bold">Mes Compétences</h3>
         <div
           ref={skillsRef}
-          className="flex flex-col sm:flex-row flex-co justify-center gap-8"
+          className="flex flex-col lg:flex-row justify-center gap-8"
         >
           {skills.map((skill, index) => (
             <div
@@ -47,20 +54,24 @@ export const Skills = () => {
                 isVisible ? "animate-fadeUp" : ""
               }`}
             >
-              <span className="w-1/5 text-lg text-black font-bold">
+              <span className="block text-lg text-black font-bold mb-4">
                 {skill.name}
               </span>
 
-              <div className="flex flex-row justify-center w-full p-4 gap-4">
+              <div className="flex flex-row justify-center w-full p-4 gap-4 flex-wrap">
                 {skill.logo.map((item, index) => (
-                  <Image
-                    src={item}
+                  <div
                     key={index}
-                    width={48}
-                    height={48}
-                    quality={100}
-                    alt="Picture of the author"
-                  />
+                    className="relative w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20"
+                  >
+                    <Image
+                      src={item}
+                      alt={`${skill.name} logo ${index + 1}`}
+                      layout="fill"
+                      objectFit="contain"
+                      quality={100}
+                    />
+                  </div>
                 ))}
               </div>
             </div>
